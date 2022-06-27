83db9aba6d75e95c42c2beb6b088cf09a116c53d
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2022-06-27 08:48:38 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -4,11 +4,12 @@ import ipaddress
 # """Задача № 2 Написать метод int32_to_ip, который принимает на вход 32-битное целое число
 # (integer) и возвращает строковое представление его в виде IPv4-адреса:"""
 
-int32 = 2149583361
-int32 = 32
-int32 = 0
+# 2149583361 ==> "128.32.10.1"
+# 32         ==> "0.0.0.32"
+# 0          ==> "0.0.0.0"
 
 
 def int32_to_ip(int32):
-  return
+    bin_str = f'{int32:b}'.rjust(32, '0')
+    return '.'.join([str(int(bin_str[idx:idx + 8], 2)) for idx in range(0, len(bin_str), 8)])
 
