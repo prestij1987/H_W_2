f4d9af686818e1aa689e1eecb67e0128b6e9ef15
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2022-06-29 13:07:00 +0300

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -55,23 +55,23 @@ def count_find_num(primesL, limit):
 
 
 
-def count_find_num(primesL, limit):
-    primes = []
-    primes_new = []
-    Primescount = 1
-    Max_primes = 0
-    for i in range(0, len(primesL)):
-        Primescount *= primesL[i]
-    for i in range(0, limit):
-        primes.append(i)
-    for i in range(0, limit-1):
-        if primes[i] != 0 and primes[i] % Primescount == 0:
-            primes_new.append(primes[i])
-    for i in range(1, (len(primes_new)-1)):
-        if primes_new[i] > primes_new[i-1]:
-            Max_primes = primes_new[i]
-    return [len(primes_new)-1, Max_primes]
+# def count_find_num(primesL, limit):
+#     primes = []
+#     primes_new = []
+#     Primescount = 1
+#     Max_primes = 0
+#     for i in range(0, len(primesL)):
+#         Primescount *= primesL[i]
+#     for i in range(0, limit):
+#         primes.append(i)
+#     for i in range(0, limit-1):
+#         if primes[i] != 0 and primes[i] % Primescount == 0:
+#             primes_new.append(primes[i])
+#     for i in range(1, (len(primes_new)-1)):
+#         if primes_new[i] > primes_new[i-1]:
+#             Max_primes = primes_new[i]
+#     return [len(primes_new)-1, Max_primes]
  
-primesL = list(map(int, input().split()))
-limit = int(input())
-print(count_find_num(primesL, limit))
+# primesL = list(map(int, input().split()))
+# limit = int(input())
+# print(count_find_num(primesL, limit))
